dd679a03c0629bedf556f86c90b843a57a280246
Ricky8955555 <397050061@qq.com>
2020-06-26 21:48:59 +0800

    修改版本号

--- Ricky8955555.CoolQ/Properties/AssemblyInfo.cs
+++ Ricky8955555.CoolQ/Properties/AssemblyInfo.cs
@@ -23,13 +23,13 @@ using System.Runtime.InteropServices;
 
 // 程序集的版本信息由下列四个值组成: 
 //
-//      主版本
-//      次版本
-//      生成号
-//      修订号
+//      主版本 - Release 版本
+//      次版本 - 结构修改 ...
+//      生成号 - 新增 App、功能 ...
+//      修订号 - Bug 修复 ...
 //
 //可以指定所有这些值，也可以使用“生成号”和“修订号”的默认值
 //通过使用 "*"，如下所示:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("0.2.1.0")]
-[assembly: AssemblyFileVersion("0.2.1.23")]
+[assembly: AssemblyVersion("0.3.0.0")]
+[assembly: AssemblyFileVersion("0.3.0.0")]
