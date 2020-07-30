1d045659a270cf03075664ded7eae06428fc7e75
Ricky8955555 <397050061@qq.com>
2020-07-30 16:06:38 +0800

    回退 CommandMessage

--- Ricky8955555.CoolQ/CommandMessage.cs
+++ Ricky8955555.CoolQ/CommandMessage.cs
@@ -6,31 +6,20 @@ namespace Ricky8955555.CoolQ
 {
     internal class CommandMessage
     {
-        public static CommandMessage Empty = new CommandMessage();
+        public string Command { get; }
 
-        public string Command { get; } = null;
-
-        public ComplexMessage Parameters { get; } = null;
-
-        public CommandMessage()
-        {
-        }
+        public ComplexMessage Parameters { get; }
 
         public CommandMessage(string command)
         {
             var splitter = new CommandMessageSplitter();
             var splitCommand = splitter.Split(command);
-            
-            if (splitCommand.Count == 1)
-            {
-                Command = splitCommand[0];
-                Parameters = null;
-            }
-            else if (splitCommand.Count > 1)
-            {
-                Command = splitCommand[0];
+            Command = splitCommand[0];
+
+            if (splitCommand.Count > 1)
                 Parameters = splitCommand.Skip(1).Select(x => ComplexMessage.Parse(x).ToMessageElement()).ToComplexMessage();
-            }
+            else
+                Parameters = null;
         }
     }
 }
